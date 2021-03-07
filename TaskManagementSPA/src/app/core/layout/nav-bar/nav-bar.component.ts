80f4c5823030d371ce71fe31b90fb53fee3504a0
Huyen Vu <quyhuyen.vu@gmail.com>
2021-03-07 13:37:56 -0600

    add transition to navbar

--- TaskManagementSPA/src/app/core/layout/nav-bar/nav-bar.component.ts
+++ TaskManagementSPA/src/app/core/layout/nav-bar/nav-bar.component.ts
@@ -1,16 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MenuService} from '../../services/menu.service';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
   @Input() isMobile = false;
-  constructor(public menuService: MenuService) { }
+  innerTop = 0;
+  constructor(public menuService: MenuService) {}
 
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  onResize(): void {
+    this.innerTop = window.pageYOffset;
   }
-
+  ngOnInit(): void {}
 }
