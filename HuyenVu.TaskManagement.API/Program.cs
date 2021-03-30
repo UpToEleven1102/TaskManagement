40bdfd4e0eb8b5e9c49bc9222dc230abcc73c389
Huyen Vu <quyhuyen.vu@gmail.com>
2021-03-30 10:38:33 -0400

    added sentry - not configured, changed connection string to azure sql server

--- HuyenVu.TaskManagement.API/Program.cs
+++ HuyenVu.TaskManagement.API/Program.cs
@@ -18,6 +18,11 @@ namespace HuyenVu.TaskManagement.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry();
+                    webBuilder.UseKestrel();
+                });
     }
 }
--- TaskManagementSPA/src/app/core/pipes/phone.pipe.ts
+++ TaskManagementSPA/src/app/core/pipes/phone.pipe.ts
@@ -4,16 +4,44 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'phone',
 })
 export class PhonePipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): unknown {
-    value = value.charAt(0) !== '+' ? value.replace('+', '') : value;
+  transform(tel: string, ...args: unknown[]): unknown {
+    const value = tel.toString().trim().replace(/^\+/, '');
 
-    let newStr = '';
-    let i = 0;
-
-    for (; i < Math.floor(value.length / 2) - 1; i++) {
-      newStr = newStr + value.substr(i * 2, 2) + '-';
+    if (value.match(/[^0-9]/)) {
+      return tel;
     }
 
-    return newStr + value.substr(i * 2);
+    var country, city, number;
+
+    switch (value.length) {
+      case 10: // +1PPP####### -> C (PPP) ###-####
+        country = 1;
+        city = value.slice(0, 3);
+        number = value.slice(3);
+        break;
+
+      case 11: // +CPPP####### -> CCC (PP) ###-####
+        country = value[0];
+        city = value.slice(1, 4);
+        number = value.slice(4);
+        break;
+
+      case 12: // +CCCPP####### -> CCC (PP) ###-####
+        country = value.slice(0, 3);
+        city = value.slice(3, 5);
+        number = value.slice(5);
+        break;
+
+      default:
+        return tel;
+    }
+
+    if (country === 1) {
+      country = '';
+    }
+
+    number = number.slice(0, 3) + '-' + number.slice(3);
+
+    return (country + ' (' + city + ') ' + number).trim();
   }
 }
